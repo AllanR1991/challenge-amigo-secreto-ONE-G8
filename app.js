3e3ff908eaa618da5cf38d05087b8f2a3b9b3e63
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-01-26 20:36:46 -0300

    feat: implementa funcionalidade de adição de amigos na lista com validação de input

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+
+function contemNumero(nome){
+    return /\d/.test(nome);
+}
+
+function adicionarAmigo(){
+    let inputNome = document.getElementById('amigo');
+    let nome = inputNome.value;
+    
+    if(nome == '' || contemNumero(nome)){
+        alert('Por favor, insira um nome valido.');
+    }else if(nome.length>1){
+        alert('Cadastrado com sucesso.');
+        amigos.push();
+        inputNome.value = '';
+    }else{
+        alert('Por favor, insira um nome.');
+    }
+}
